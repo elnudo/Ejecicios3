ed8c58df5222a3a8590c2c9bf278b0521a60a08d
Marco Moreno <marcomore@gmail.com>
2023-02-20 16:26:25 -0500

    Primer ejercicio del bootCampEspanol

---
+++ src/com/marcomoreno/Main.java
@@ -0,0 +1,19 @@
+package com.marcomoreno;
+
+import java.sql.SQLOutput;
+
+public class Main {
+    public static void main(String[] args) {
+        ejercicio3(27,46, 81)
+    }
+
+    public static int ejercicio3(int a, int b, int c) {
+
+    }
+
+}
+
+public class coche {
+    int numeroPuertas;
+    public static int
+}
